72d89c37c83c5adea37a31f0e7a3b5b1849a4226
Fernando-Sosa177 <fernandososa.910k@gmail.com>
2025-03-05 20:15:33 -0300

    Proyect

--- pokemon/src/hook/usepokemones.js
+++ pokemon/src/hook/usepokemones.js
@@ -6,16 +6,16 @@ function usePokemones(){
     const   [siguienteUrl , setSiguienteUrl] = useState('')
     const   [verMas, setVerMas] = useState(true)
 
-    const getPokemones = async (url = URL_DEFAULT) => {
+    let getPokemones = async (url = URL_DEFAULT) => {
         // Recuperamos la lista de pokemones
-        const response = await fetch(url)
-        const listaPokemones = await response.json()
-        const { next,results} = listaPokemones // Guardamos el result
+        let response = await fetch(url)
+        let listaPokemones = await response.json()
+        let { next,results} = listaPokemones // Guardamos el result
         
-        const newPokemones = await Promise.all(
+        let newPokemones = await Promise.all(
             results.map(async (pokemon) => {
-                const response = await fetch(pokemon.url)
-                const poke = await response.json()
+                let response = await fetch(pokemon.url)
+                let poke = await response.json()
         
                 return {
                 name: poke.name,
@@ -27,14 +27,14 @@ function usePokemones(){
         return {next, newPokemones}
         }
 
-        const obtenerPokemones = async () => {
-            const {next, newPokemones} = await getPokemones()
+        let obtenerPokemones = async () => {
+            let {next, newPokemones} = await getPokemones()
             setPokemones(newPokemones)
             setSiguienteUrl(next)
         }
 
-        const masPokemones = async () => {
-            const {next, newPokemones} = await getPokemones(siguienteUrl)
+        let masPokemones = async () => {
+            let {next, newPokemones} = await getPokemones(siguienteUrl)
             setPokemones(prev => [...prev, ...newPokemones])
             next === null && setVerMas(false)
             setSiguienteUrl(next)

21cf96ffad9fbacbd94b644d677c176bcf7c6bd9
Fernando-Sosa177 <fernandososa.910k@gmail.com>
2025-03-17 19:34:40 -0300

    ProyectoFinal

--- pokemon/src/Nav/navbar.jsx
+++ pokemon/src/Nav/navbar.jsx
@@ -1,36 +1,34 @@
 import { useEffect, useState } from 'react';
 import { Logo, Luna, Sol } from '../icons.jsx';
-import {Link} from 'react-router-dom'
-import './navbar.css'
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
+    let [tema, setTema] = useState('claro');
 
-    let [ tema, setTema ] = useState ('claro')
+    let handleChange = (e) => setTema(e.target.checked ? 'oscuro' : 'claro');
 
-    let handleChange = (e) => setTema(e.target.checked ? 'oscuro' : 'claro')
+    useEffect(() => {
+        document.body.setAttribute('data-tema', tema);
+    }, [tema]);
 
-    useEffect (() => {
-        document.body.setAttribute('data-tema', tema)
-    }, [tema])
-    
     return (
         <nav>
-        <Logo className="logo"/>
-        <ul>
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="#">Favoritos</Link></li>
-            <li><Link to="/Registrodex">Registrarse</Link></li>
-        </ul>
-        <div className='switch'>
-            <Sol/>
-            <label>
-                <input type='checkbox' className='check-switch' onChange={handleChange} hidden/>
-                <span className='slider'></span>
-            </label>
-            <Luna/>
-        </div>
+            <Logo className="logo" />
+            <ul>
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/Registrodex">Registrarse</Link></li>
+            </ul>
+            <div className='switch'>
+                <Sol />
+                <label>
+                    <input type='checkbox' className='check-switch' onChange={handleChange} hidden />
+                    <span className='slider'></span>
+                </label>
+                <Luna />
+            </div>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
